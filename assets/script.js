f5f2eb72e7a303893c2f1d0d3c458b8d84efa57f
Steven Sammon <stevenesammon@hotmail.com>
2017-12-05 11:10:18 -0500

    adds js file. Creates basic html

---
+++ assets/script.js
@@ -0,0 +1,42 @@
+$( document ).ready(function() {
+
+
+
+$("#letsgo").on("click", function ( event ) {
+
+	$("#homepage").html(  )
+
+})
+
+
+queryUrl = ""
+
+
+$.ajax ({
+ url: queryUrl
+ method: "GET"
+
+}).done(function(res) {
+
+console.log(res);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}); // closes document ready
